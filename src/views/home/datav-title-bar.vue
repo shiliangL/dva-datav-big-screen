1a06ad329e4f2858c49c865331c3b9e4b0abfd92
shiliangwei <1462999406@qq.com>
2022-10-29 11:36:54 +0800

    2022年10月29日

--- src/views/home/datav-title-bar.vue
+++ src/views/home/datav-title-bar.vue
@@ -27,6 +27,14 @@ export default {
   justify-content: center;
   height: 54px;
   width: 100%;
+  // mask-image: linear-gradient(
+  //   160deg,
+  //   rgba(0, 0, 0, 0.66) 100%,
+  //   transparent 100%
+  // );
+  // 增加遮罩蒙版
+  mix-blend-mode: overlay; // 增加颜色混合模式
+
   animation-fill-mode: both;
   -webkit-animation-fill-mode: both;
   // box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
@@ -47,9 +55,48 @@ export default {
     color: #fff;
     font-weight: 600;
     font-size: 1.2rem;
-    // -webkit-background-clip: text;
-    // -webkit-text-fill-color: transparent;
-    // background-image: linear-gradient(135deg, #5efce8 10%, #736efe);
+  }
+
+  &::after {
+    content: " ";
+    display: block;
+    position: absolute;
+    top: 0;
+    width: 126px;
+    height: 95%;
+    display: block;
+    position: absolute;
+    background-image: linear-gradient(
+      126deg,
+      transparent 30%,
+      #dbf3fb 48%,
+      #e4f9ff 50%,
+      #f9f9f9 52%,
+      transparent 70%
+    ); // 反光元素
+    animation: scanning 3.5s infinite linear;
+    opacity: 0.7;
+  }
+}
+
+@keyframes scanning {
+  0% {
+    left: -20px;
+    opacity: 0;
+  }
+  10% {
+    opacity: 0.7;
+  }
+  70% {
+    opacity: 0.7;
+  }
+  80% {
+    left: 90%;
+    opacity: 0;
+  }
+  100% {
+    right: 0px;
+    opacity: 0;
   }
 }
 </style>
